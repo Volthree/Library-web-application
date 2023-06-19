8bd3a18e5fb4a30b9b46b3a80ff870403d30edaf
Anton Lunev <email@alunev.org>
2023-06-18 22:15:09 -0700

    example test

--- src/test/java/HumanControllerTest.java
+++ src/test/java/HumanControllerTest.java
@@ -1,15 +1,23 @@
 import application.library.config.MySpringMVCDispatcherServletInit;
 import application.library.config.SpringConfig;
+import application.library.controllers.HumanController;
 import application.library.dao.HumanDAO;
 import application.library.models.Human;
 import application.library.util.HumanValidator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
+import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class HumanControllerTest {
     @Mock
     MySpringMVCDispatcherServletInit mySpringMVCDispatcherServletInit;
@@ -23,11 +31,27 @@ public class HumanControllerTest {
     Model model;
 
     @Test
-    public void showAllHumanTest(){
+    public void showAllHumanReturnsEmptyList() {
+        HumanController humanController = new HumanController(humanDAO, humanValidator);
 
-        humanDAO = new HumanDAO(new JdbcTemplate(springConfig.dataSource()));
-        List<Human> humanList = humanDAO.getAllHumans();
-        System.out.println(humanList);
+        ExtendedModelMap model1 = new ExtendedModelMap();
+
+        humanController.getAllHuman(model1);
+
+        assertThat(model1.getAttribute("allHuman")).isEqualTo(Collections.emptyList());
+    }
+
+    @Test
+    public void showAllHumanReturnsListOfOneHuman() {
+        List<Human> humanList = List.of(new Human());
+        when(humanDAO.getAllHumans()).thenReturn(humanList);
+
+        HumanController humanController = new HumanController(humanDAO, humanValidator);
+
+        ExtendedModelMap model1 = new ExtendedModelMap();
+        humanController.getAllHuman(model1);
+
+        assertThat(model1.getAttribute("allHuman")).isEqualTo(humanList);
     }
 
 }
